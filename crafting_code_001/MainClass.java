a5cefbca583b56560493bd73830d07c6a94c9ab2
FOSSALGO <60698625+FOSSALGO@users.noreply.github.com>
2020-12-31 22:15:38 +0800

    Create MainClass.java

---
+++ crafting_code_001/MainClass.java
@@ -0,0 +1,8 @@
+public class MainClass {
+    public static void main(String[]args){
+        System.out.println("Halo Kawan-Kawan");
+        System.out.println("Apa Kabar?");
+        System.out.println("Kabar Baik");
+        System.out.println("Terimakasih");
+    }
+}
