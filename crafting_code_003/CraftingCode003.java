883efd46ddcdd22f2d9366523d561e1363c1814b
FOSSALGO <60698625+FOSSALGO@users.noreply.github.com>
2021-01-03 19:23:36 +0800

    Create CraftingCode003.java

---
+++ crafting_code_003/CraftingCode003.java
@@ -0,0 +1,7 @@
+public class CraftingCode003 {
+    public static void main(String[] fosa){ 
+        for(int i=1;i<=1000;i++){
+            System.out.println("Text yang Sama "+i);
+        }
+    }
+}
