64e2366013635cd50e01f212a0957510f65acf55
FOSSALGO <60698625+FOSSALGO@users.noreply.github.com>
2021-01-03 21:58:49 +0800

    Create CraftingCode004.java

---
+++ crafting_code_004/CraftingCode004.java
@@ -0,0 +1,9 @@
+public class CraftingCode004 {
+    public static void main(String[] args) {
+        System.out.print("Hello World\n");
+        System.out.print("Halo BosQ\n");
+        System.out.print("Halo Kawan");
+        System.out.println();
+        
+    }
+}
