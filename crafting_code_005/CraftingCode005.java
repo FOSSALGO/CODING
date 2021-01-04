5003ee9701fe7992326a98b123d4e67ac3e3af0b
FOSSALGO <60698625+FOSSALGO@users.noreply.github.com>
2021-01-04 13:49:36 +0800

    Create CraftingCode005.java

---
+++ crafting_code_005/CraftingCode005.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class CraftingCode005 {
+    public static void main(String[] args) {
+        System.out.println("INPUT");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("OUTPUT");
+        for(int i=1;i<=n;i++){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+}
