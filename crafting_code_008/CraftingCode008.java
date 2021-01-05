968532f20a7ae8f4d0f633850e00040b51da283d
FOSSALGO <60698625+FOSSALGO@users.noreply.github.com>
2021-01-05 20:07:28 +0800

    Create CraftingCode008.java

---
+++ crafting_code_008/CraftingCode008.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class CraftingCode008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("INPUT");
+        String myStr = sc.nextLine();
+        System.out.println("OUTPUT");
+        System.out.println(myStr);
+    }
+}
