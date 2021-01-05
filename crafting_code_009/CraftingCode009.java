2c62113d4cb8b09d9c7184865b717d3768549684
FOSSALGO <60698625+FOSSALGO@users.noreply.github.com>
2021-01-05 21:48:34 +0800

    Create CraftingCode009.java

---
+++ crafting_code_009/CraftingCode009.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class CraftingCode009 {
+    public static void main(String[] args) {
+        Scanner scw = new Scanner(System.in);
+        System.out.println("INPUT");
+        String myStr = scw.nextLine();
+        System.out.println("OUTPUT");
+        int panjangString = myStr.length();
+        System.out.print("panjang string yang di-INPUT: ");
+        System.out.println(panjangString);        
+    }
+}
