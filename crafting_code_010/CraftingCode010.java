dd4a7fe82655cecf01400409d390d8ec40b06245
FOSSALGO <60698625+FOSSALGO@users.noreply.github.com>
2021-01-07 12:19:37 +0800

    Create CraftingCode010.java

---
+++ crafting_code_010/CraftingCode010.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CraftingCode010 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("INPUT");
+        String myStr = scn.nextLine();
+        int beginIndex = scn.nextInt();
+        int endIndex = scn.nextInt();
+        String subStr = myStr.substring(beginIndex, endIndex);
+        System.out.println("OUTPUT");
+        System.out.println("string input: "+myStr);   
+        System.out.println("beginIndex  : "+beginIndex);
+        System.out.println("substring   : "+subStr);
+    }
+}
