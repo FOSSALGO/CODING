e09afba62311fb0ba4b627c4db691b48f7711bb6
FOSSALGO <60698625+FOSSALGO@users.noreply.github.com>
2021-01-07 12:20:42 +0800

    Update CraftingCode010.java

--- crafting_code_010/CraftingCode010.java
+++ crafting_code_010/CraftingCode010.java
@@ -7,10 +7,12 @@ public class CraftingCode010 {
         String myStr = scn.nextLine();
         int beginIndex = scn.nextInt();
         int endIndex = scn.nextInt();
+        //String subStr = myStr.substring(beginIndex);
         String subStr = myStr.substring(beginIndex, endIndex);
         System.out.println("OUTPUT");
         System.out.println("string input: "+myStr);   
         System.out.println("beginIndex  : "+beginIndex);
+        System.out.println("endIndex    : "+endIndex);
         System.out.println("substring   : "+subStr);
     }
 }
