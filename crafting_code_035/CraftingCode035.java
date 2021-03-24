58cbe02163e1e09d77395460c4bda6bc6b7da070
FOSSALGO <60698625+FOSSALGO@users.noreply.github.com>
2021-03-24 08:16:57 +0800

    Create CraftingCode035.java

---
+++ crafting_code_035/CraftingCode035.java
@@ -0,0 +1,181 @@
+package fosalgo;
+
+import java.util.ArrayList;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class CraftingCode035 extends Application implements Runnable {
+
+    //Loop Parameters
+    private final static int MAX_FPS = 60;
+    private final static int MAX_FRAME_SKIPS = 10;
+    private final static int FRAME_PERIOD = 1000 / MAX_FPS;
+
+    //Thread
+    private Thread thread;
+    private volatile boolean running = true;
+
+    //Canvas
+    Canvas canvas = null;
+
+    //KEYBOARD HANDLER
+    ArrayList<String> inputKeyboard = new ArrayList<String>();
+
+    //variabel untuk komponen-komponen canvas
+    ArrayList<double[]> titik = new ArrayList<>();//titik[0] = x; titik[1]=y
+    
+    public CraftingCode035() {
+        resume();
+    }
+
+    //THREAD
+    private void resume() {
+        reset();
+        thread = new Thread(this);
+        running = true;
+        thread.start();
+    }
+
+    //THREAD
+    private void pause() {
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //LOOP
+    private void reset() {
+
+    }
+
+    //LOOP
+    private void update() {
+
+    }
+
+    //LOOP
+    private void draw() {
+        try {
+            if (canvas != null) {
+                GraphicsContext gc = canvas.getGraphicsContext2D();
+                gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+                
+                if(titik!=null){
+                    for(int i=0;i<titik.size();i++){
+                        double[] p = titik.get(i);
+                        double x = p[0];
+                        double y = p[1];
+                        gc.setFill(Color.valueOf("#d35400"));
+                        double r = 10;
+                        gc.fillOval(x-r, y-r, 2*r, 2*r);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        long beginTime;
+        long timeDiff;
+        int sleepTime = 0;
+        int framesSkipped;
+        //LOOP WHILE running = true; 
+        while (running) {
+            try {
+                synchronized (this) {
+                    beginTime = System.currentTimeMillis();
+                    framesSkipped = 0;
+                    update();
+                    draw();
+                }
+                timeDiff = System.currentTimeMillis() - beginTime;
+                sleepTime = (int) (FRAME_PERIOD - timeDiff);
+                if (sleepTime > 0) {
+                    try {
+                        Thread.sleep(sleepTime);
+                    } catch (InterruptedException e) {
+                    }
+                }
+                while (sleepTime < 0 && framesSkipped < MAX_FRAME_SKIPS) {
+                    update();
+                    sleepTime += FRAME_PERIOD;
+                    framesSkipped++;
+                }
+            } finally {
+
+            }
+        }
+    }
+
+    public void setHandling(Scene scene) {
+        //HANDLING KEYBOARD EVENT
+        scene.setOnKeyPressed(
+                new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent e) {
+                String code = e.getCode().toString();
+                if (!inputKeyboard.contains(code)) {
+                    inputKeyboard.add(code);
+                    System.out.println(code);
+                }
+            }
+        }
+        );
+
+        scene.setOnKeyReleased(
+                new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent e) {
+                String code = e.getCode().toString();
+                inputKeyboard.remove(code);
+            }
+        }
+        );
+
+        //HANDLING MOUSE EVENT
+        scene.setOnMouseClicked(
+                new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent e) {
+                double x = e.getX();
+                double y = e.getY();
+                double[] p = {x,y};
+                titik.add(p);
+            }
+        }
+        );
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root);
+        setHandling(scene);
+
+        canvas = new Canvas(1024, 700);
+        root.getChildren().add(canvas);
+
+        primaryStage.setTitle("FOSALGO - Indonesia");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
