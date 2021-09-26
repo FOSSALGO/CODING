282d8bef95c5cead0be051d0acb6b26c05958138
FOSSALGO <60698625+FOSSALGO@users.noreply.github.com>
2021-09-27 01:57:58 +0800

    Create CraftingCode_045.java

---
+++ crafting_code_045/CraftingCode_045.java
@@ -0,0 +1,9 @@
+package fosalgo;
+
+public class CraftingCode_045 {
+    public static void main(String[] args) {
+        Persegi prs = new Persegi("Biru",24.7);
+        String nilai = prs.toString();
+        System.out.println(nilai);
+    }
+}
