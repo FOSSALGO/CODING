37dee68ac84a65c1b38fd275be021c99ad1670a2
FOSSALGO <60698625+FOSSALGO@users.noreply.github.com>
2021-09-27 20:17:09 +0800

    Create CraftingCode_046.java

---
+++ crafting_code_046/CraftingCode_046.java
@@ -0,0 +1,216 @@
+package fosalgo;
+
+import java.util.ArrayList;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class CraftingCode_046 extends Application implements Runnable {
+
+    //Loop Parameters
+    private final static int MAX_FPS = 60;
+    private final static int MAX_FRAME_SKIPS = 5;
+    private final static int FRAME_PERIOD = 1000 / MAX_FPS;
+
+    //Thread
+    private Thread thread;
+    private volatile boolean running = true;
+
+    //Canvas
+    Canvas canvas = null;
+
+    //KEYBOARD HANDLER
+    ArrayList<String> inputKeyboard = new ArrayList<String>();
+
+    //VARIABEL GERAK VERTIKAL
+    float cx, cy, radius, hMIN, hMAX;
+            
+    //VARIABEL GRAVITASI
+    float g =  04f;//percepatan gravitasi
+    float t, v;
+    boolean turun = true;//untuk gerak vertikal ke atas maka turun = false
+    
+    //VARIABEL GERAK VERTIKAL ATAS
+    float vUP = 5f;//Vo untuk gerak Vertikal ke Atas
+            
+    public CraftingCode_046() {
+        resume();
+    }
+
+    //THREAD
+    private void resume() {
+        reset();
+        thread = new Thread(this);
+        running = true;
+        thread.start();
+    }
+
+    //THREAD
+    private void pause() {
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //LOOP
+    private void reset() {
+
+    }
+
+    //LOOP
+    private void update() {
+        if (inputKeyboard.contains("SPACE")) {
+            turun = false;
+            t = 0;
+            v = vUP;
+            hMAX = - v * v / 2 * g;
+        }
+        
+        if (turun) {
+            //GV TURUN
+            t++;
+            v = v + (g*t);
+            cy += v;
+        }else{
+            //GV ATAS
+            t++;
+            v = v - g*t;
+            cy -=v;
+        }
+        
+        //TEPI
+        if (cy > hMIN) {
+            t = 0;
+            v = 0;
+            cy = hMIN;
+        }
+        
+        if (cy < hMAX) {
+            t = 0;
+            v = 0;
+            cy = hMAX;
+            turun = true;
+        }
+        
+    }
+
+    //LOOP
+    private void draw() {
+        try {
+            if (canvas != null) {
+                GraphicsContext gc = canvas.getGraphicsContext2D();
+                gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+                
+                //MENGGAMBAR LINGKARAN
+                gc.setFill(Color.ORANGERED);
+                gc.fillOval(cx, cy, radius, radius);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Group root = new Group();
+        Scene scene = new Scene(root);
+
+        //INISIALISASI canvas dan variabel gerak jatuh bebas
+        canvas = new Canvas(480, 640);
+        cx = (float) (canvas.getWidth() * 0.5);
+        cy = 0;
+        radius = 50;
+        hMIN = (float) (canvas.getHeight() - radius);
+        hMAX = 0 + radius;
+        root.getChildren().add(canvas);
+
+        //HANDLING KEYBOARD EVENT
+        scene.setOnKeyPressed(
+                new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent e) {
+                String code = e.getCode().toString();
+                if (!inputKeyboard.contains(code)) {
+                    inputKeyboard.add(code);
+                    System.out.println(code);
+                }
+            }
+        }
+        );
+
+        scene.setOnKeyReleased(
+                new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent e) {
+                String code = e.getCode().toString();
+                inputKeyboard.remove(code);
+            }
+        }
+        );
+
+        //HANDLING MOUSE EVENT
+        scene.setOnMouseClicked(
+                new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent e) {
+
+            }
+        }
+        );
+
+        primaryStage.setTitle("Gerak Vertikal");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void run() {
+        long beginTime;
+        long timeDiff;
+        int sleepTime = 0;
+        int framesSkipped;
+        //LOOP WHILE running = true; 
+        while (running) {
+            try {
+                synchronized (this) {
+                    beginTime = System.currentTimeMillis();
+                    framesSkipped = 0;
+                    update();
+                    draw();
+                }
+                timeDiff = System.currentTimeMillis() - beginTime;
+                sleepTime = (int) (FRAME_PERIOD - timeDiff);
+                if (sleepTime > 0) {
+                    try {
+                        Thread.sleep(sleepTime);
+                    } catch (InterruptedException e) {
+                    }
+                }
+                while (sleepTime < 0 && framesSkipped < MAX_FRAME_SKIPS) {
+                    update();
+                    sleepTime += FRAME_PERIOD;
+                    framesSkipped++;
+                }
+            } finally {
+
+            }
+        }
+    }
+
+}
