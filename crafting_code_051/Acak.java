1d91bfa8fa62bba175b858c96557df8e63cf0227
FOSSALGO <60698625+FOSSALGO@users.noreply.github.com>
2021-10-02 08:23:25 +0800

    Create Acak.java

---
+++ crafting_code_051/Acak.java
@@ -0,0 +1,31 @@
+package ledakanpartikel;
+
+public class Acak {
+    
+    static int acakInt(int min, int max) {
+        if(min>max){
+            int temp = min;
+            min = max;
+            max = temp;
+        }
+        return (int) (min + Math.random() * (max - min + 1));
+    }
+    
+    static float acakFloat(float min, float max) {
+        if(min>max){
+            float temp = min;
+            min = max;
+            max = temp;
+        }
+        return (float) (min + (max - min) * Math.random());
+    }
+    
+    static double acakDouble(double min, double max) {
+        if(min>max){
+            double temp = min;
+            min = max;
+            max = temp;
+        }
+        return min + (max - min) * Math.random();
+    }
+}
